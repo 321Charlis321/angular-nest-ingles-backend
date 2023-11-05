2637430bbebe91670b6a22d3e03cf6c30dbfcf47
Frankzua Charlis <ch.ar.lis@hotmail.com>
2023-11-05 13:22:58 -0500

    Port added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { log } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,9 +12,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
-
+  const PORT = process.env.PORT ?? 3000;
+  console.log(`App corriendo en el port ${PORT}`)
   app.enableCors();//Esto es importante para que no se tenga fallas a la hora entre la comunicacion del backend y fronted
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
 }
 bootstrap();
 
